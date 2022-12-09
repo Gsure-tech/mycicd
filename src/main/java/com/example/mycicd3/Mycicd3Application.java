6a347ae7e20bd89c093a065c09d9f34ccda7e1dd
gsure-tech <Geeescode@>
2022-12-09 08:04:00 +0100

    code

---
+++ src/main/java/com/example/mycicd3/Mycicd3Application.java
@@ -0,0 +1,27 @@
+package com.example.mycicd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Mycicd3Application {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return"welcome to javatechie !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mycicd3Application.class, args);
+    }
+//    echo "# mycicd" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/Gsure-tech/mycicd.git
+//    git push -u origin main
+
+}
---
+++ src/test/java/com/example/mycicd3/Mycicd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycicd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycicd3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
